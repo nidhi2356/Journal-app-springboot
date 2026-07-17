7a3b67be08f07b9e2cbd4a8c2f2548ffe0fd5ccd
nidhi2356 <nidhisharma00200@gmail.com>
2026-07-17 23:17:24 +0530

    refactor: replace getters and setters with Lombok

--- src/main/java/com/journal_app/java/entity/JournalEntry.java
+++ src/main/java/com/journal_app/java/entity/JournalEntry.java
@@ -1,11 +1,15 @@
 package com.journal_app.java.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.time.LocalDateTime;
 
+
+@Getter
+@Setter
 @Document(collection = "journal_entries")
 public class JournalEntry {
     @Id
