eb7abf1966b418964b92de6ee205189a0169d11c
nidhi2356 <nidhisharma00200@gmail.com>
2026-07-17 13:40:49 +0530

    feat: Add GET endpoint to fetch all journal entries

---
+++ src/main/java/com/journal_app/java/entity/JournalEntry.java
@@ -0,0 +1,42 @@
+package com.journal_app.java.entity;
+
+import java.time.LocalDateTime;
+
+public class JournalEntry {
+    private long id;
+    private String title;
+    private String content;
+    private LocalDateTime date;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+}
