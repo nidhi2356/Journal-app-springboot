dca0f5497a790d50b945aff9bdace6c9e43d32e5
nidhi2356 <nidhisharma00200@gmail.com>
2026-07-17 23:28:01 +0530

    refactor: shift date initialization logic to service layer

--- src/main/java/com/journal_app/java/service/JournalEntryService.java
+++ src/main/java/com/journal_app/java/service/JournalEntryService.java
@@ -2,13 +2,17 @@ package com.journal_app.java.service;
 
 import com.journal_app.java.entity.JournalEntry;
 import com.journal_app.java.repository.JournalEntryRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+
+@Slf4j
 @Component
 public class JournalEntryService {
 
@@ -16,8 +20,13 @@ public class JournalEntryService {
     private JournalEntryRepository journalEntryRepository;
 
 
-    public void saveEntry(JournalEntry journalEntry){
-        journalEntryRepository.save(journalEntry);
+    public void saveEntry(JournalEntry journalEntry) {
+        try {
+            journalEntry.setDate(LocalDateTime.now());
+            journalEntryRepository.save(journalEntry);
+        } catch (Exception e) {
+            log.error("Exception ", e);
+        }
     }
 
     public List<JournalEntry> getAll(){
