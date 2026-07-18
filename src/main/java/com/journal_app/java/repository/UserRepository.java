7ebcd4823fbbf17427f5a5dfd5d3b3e0fee516b4
nidhi2356 <nidhisharma00200@gmail.com>
2026-07-18 11:05:13 +0530

    feat: add UserRepository for user data access

---
+++ src/main/java/com/journal_app/java/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.journal_app.java.repository;
+
+import com.journal_app.java.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+
+}
