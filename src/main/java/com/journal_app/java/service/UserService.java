8e0f987d771e0e04b4dc75f2e8c68670f33a7f81
nidhi2356 <nidhisharma00200@gmail.com>
2026-07-18 11:05:32 +0530

    feat: add UserService for user business logic

---
+++ src/main/java/com/journal_app/java/service/UserService.java
@@ -0,0 +1,39 @@
+package com.journal_app.java.service;
+
+import com.journal_app.java.entity.JournalEntry;
+import com.journal_app.java.entity.User;
+import com.journal_app.java.repository.JournalEntryRepository;
+import com.journal_app.java.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+
+@Slf4j
+@Component
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public void saveEntry(User user) {
+        userRepository.save(user);
+    }
+
+    public List<User> getAll(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(ObjectId id){
+        return userRepository.findById(id);
+    }
+
+    public void deleteById(ObjectId id){userRepository.deleteById(id);
+    }
+}
