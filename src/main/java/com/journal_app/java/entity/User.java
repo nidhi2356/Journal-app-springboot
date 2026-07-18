c77b319364cddee5e203b425592bf87d7246ff4d
nidhi2356 <nidhisharma00200@gmail.com>
2026-07-18 11:08:09 +0530

    feat: add MongoDB annotations to User entity

--- src/main/java/com/journal_app/java/entity/User.java
+++ src/main/java/com/journal_app/java/entity/User.java
@@ -1,15 +1,19 @@
 package com.journal_app.java.entity;
 
+import lombok.Data;
 import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class user {
+@Document(collection = "users")
+@Data
+public class User {
 
     @Id
     private ObjectId id;
