88434eb3bbd8bdcf40e1d4082dc16622155b53ce
nidhi2356 <nidhisharma00200@gmail.com>
2026-07-18 10:39:43 +0530

    chore: restore JavaApplication after testing

--- src/main/java/com/journal_app/java/JavaApplication.java
+++ src/main/java/com/journal_app/java/JavaApplication.java
@@ -1,26 +1,7 @@
 package com.journal_app.java;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-/*
-
-@SpringBootApplication
-public class JavaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JavaApplication.class, args);
-	}
-
-}
-
- */
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class JavaApplication {
